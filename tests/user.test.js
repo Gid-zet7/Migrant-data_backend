6bf86ebce65e2ee7da154f8d40276c8216764aa7
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-05 20:28:41 +0000

    Tested user update function

--- tests/user.test.js
+++ tests/user.test.js
@@ -10,7 +10,7 @@ describe("POST /user/create", () => {
           username: "username",
           password: "123456789",
           email: "username@example.com",
-          roles: "User",
+          roles: ["User"],
         });
       expect(response.statusCode).toBe(200);
     });
@@ -22,7 +22,7 @@ describe("POST /user/create", () => {
           username: "username",
           password: "password",
           email: "username@example.com",
-          roles: "User",
+          roles: ["User"],
         });
       expect(response.headers["content-type"]).toEqual(
         expect.stringContaining("json")
@@ -37,6 +37,48 @@ describe("POST /user/create", () => {
       for (const body of bodyData) {
         const response = await request(userControllerTest)
           .post("/user/create")
+          .send(body);
+        expect(response.statusCode).toBe(400);
+      }
+    });
+  });
+});
+
+describe("PATCH /user/update", () => {
+  describe("given an id, username, email, password and roles", () => {
+    test("Should respond with a 220 status code", async () => {
+      const response = await request(userControllerTest)
+        .patch("/user/update")
+        .send({
+          id: 1098532,
+          username: "username",
+          password: "123456789",
+          email: "username@example.com",
+          roles: ["User"],
+        });
+      expect(response.statusCode).toBe(220);
+    });
+
+    test("Should respond with a 200 status code if password is not provided", async () => {
+      const response = await request(userControllerTest)
+        .patch("/user/update")
+        .send({
+          id: 1098532,
+          username: "username",
+          email: "username@example.com",
+          roles: ["User"],
+        });
+      expect(response.statusCode).toBe(200);
+    });
+  });
+
+  describe("When the username, id, email or roles is missing", () => {
+    test("Should respond with a 400 status code", async () => {
+      const bodyData = [{ username: "username" }, { password: "123456789" }];
+
+      for (const body of bodyData) {
+        const response = await request(userControllerTest)
+          .patch("/user/update")
           .send(body);
         expect(response.statusCode).toBe(400);
       }
--- tests/userControllerTest.js
+++ tests/userControllerTest.js
@@ -10,8 +10,34 @@ userControllerTest.post(
   asyncHandler(async (req, res) => {
     const { username, email, password, roles } = req.body;
 
-    if (!username || !password || !email || !roles) {
+    if (
+      !username ||
+      !password ||
+      !email ||
+      !Array.isArray(roles) ||
+      !roles.length
+    ) {
       return res.sendStatus(400);
+    }
+
+    res.send({ message: "Good" });
+    res.sendStatus(200);
+  })
+);
+
+userControllerTest.patch(
+  "/user/update",
+  asyncHandler(async (req, res) => {
+    const { id, username, email, password, roles } = req.body;
+
+    // Allow update even if password is not icluded
+    if (!id || !username || !email || !Array.isArray(roles) || !roles.length) {
+      return res.sendStatus(400);
+    }
+
+    // if user decides to change password
+    if (password) {
+      return res.sendStatus(220);
     }
 
     res.send({ message: "Good" });
