4c35ac9ce77136a98cf2ba5d8c8bb62e94fbc93e
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-05 04:37:46 +0000

    Changed user to migrant_id( makes more sense)

--- models/dataForm.js
+++ models/dataForm.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const DataFormModel = new Schema(
   {
-    user: {
+    migrant_id: {
       type: Schema.Types.ObjectId,
       ref: "MigrantInfo",
       required: true,
