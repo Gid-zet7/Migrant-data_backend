9a17f32fffc6399ceaaa978f51bdc1b5a5c44844
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-05 17:36:32 +0000

    Updated routes to allow users to perform crud operations

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/", userController.user_list);
+router.post("/", userController.user_create);
+router.delete("/", userController.user_delete);
 
 module.exports = router;
