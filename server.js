3e75d4918bfb5f0e3f36d1faa22a4ef316dee2b6
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-07 23:14:10 +0000

    Modified the server to use the new logger middleware

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const { logger } = require("./middleware/logger");
 
 var indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
@@ -12,7 +12,7 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
-app.use(logger("dev"));
+app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
