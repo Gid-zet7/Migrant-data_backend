4a0c772bba1c51cd222b9f527efab4c6c2a6fcee
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-08 03:26:05 +0000

    Added the verify jwt middleware to verify user requests

--- routes/migrantData.js
+++ routes/migrantData.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const dataController = require("../controllers/dataController");
+const dataController = require("../controllers/dataFormController");
+const verifyJWT = require("../middleware/verifyJWT");
+
+router.use(verifyJWT);
 
 router.get("/", dataController.get_all_migrant_data);
 router.post("/", dataController.migrant_data_create);
