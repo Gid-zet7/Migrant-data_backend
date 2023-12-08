53c672cabee008c0d1b6af892074bb91382253b8
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-08 03:42:56 +0000

    Modified server to use the migrant data form route and also route users to the 404 html page if request is undefined/unknown

--- server.js
+++ server.js
@@ -28,24 +28,19 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
-app.use("/data", migrantDataRouter);
+app.use("/dataform", migrantDataRouter);
 app.use("/migrant", migrantInfoRouter);
 app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 not Found");
+  }
 });
 
 const PORT = 8080;
