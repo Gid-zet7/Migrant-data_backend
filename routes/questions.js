2228aaa14b508a1c6b4162ade87b316a67172245
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-11 02:00:48 +0000

    Added the question forms routes

---
+++ routes/questions.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const questionsFormController = require("../controllers/questionsFormController");
+const verifyJWT = require("../middleware/verifyJWT");
+
+// router.use(verifyJWT);
+
+router.get("/", questionsFormController.get_all_questionsForm);
+router.post("/", questionsFormController.question_form_create);
+router.delete("/", questionsFormController.question_form_delete);
+
+module.exports = router;
