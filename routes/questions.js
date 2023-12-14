ddc13ce9054a0cb964300725b006c2b73c5aaadd
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-14 21:31:33 +0000

    Added verify JWT to protect the routes

--- routes/questions.js
+++ routes/questions.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const questionsFormController = require("../controllers/questionsFormController");
 const verifyJWT = require("../middleware/verifyJWT");
 
-// router.use(verifyJWT);
+router.use(verifyJWT);
 
 router.get("/", questionsFormController.get_all_questionsForm);
 router.post("/", questionsFormController.question_form_create);
